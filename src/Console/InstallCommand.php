b3cc5401e961ca250f6fa9068427f0e91ed71b65
Quentin 'kay' S <quentin.salerne@gmail.com>
2022-08-20 01:19:58 +0200

    Version 0.1; Need to add parameters; TypeScript; and maybe do file edit instead of copy

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace QuentinKay\LaravelTemplate\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use RuntimeException;
+use Symfony\Component\Process\Process;
+
+class InstallCommand extends Command
+{
+    protected $signature = 'laravel-template:install';
+    protected $description = 'Install laravel template.';
+
+    public function handle()
+    {
+        // NPM Packages...
+        $this->info('Add npm deps..');
+        $this->updateNodePackages(function ($packages) {
+            return [
+                    '@vitejs/plugin-vue' => '^3.0.0',
+                    '@inertiajs/inertia' => '^0.11.0',
+                    '@inertiajs/inertia-vue3' => '^0.6.0',
+                    '@inertiajs/progress' => '^0.2.0',
+                    'vue' => '^3.2.0',
+                    'sass' => '^1.54.0',
+                ] + $packages;
+        });
+
+        // Middleware...
+        $this->info('Install Middlewares..');
+        $this->installMiddlewareAfter('SubstituteBindings::class', '\App\Http\Middleware\HandleInertiaRequests::class');
+        Storage::copy(__DIR__.'/../../stubs/app/Http/Middleware/HandleInertiaRequests.php', app_path('Http/Middleware/HandleInertiaRequests.php'));
+
+        // Resources..
+        $this->info('Install Resources..');
+
+        // CSS + SASS..
+        Storage::delete(resource_path('css/app.css'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('scss'));
+        Storage::copy(__DIR__.'/../../stubs/resources/scss/app.scss', resource_path('scss/app.scss'));
+
+        // Views..
+        //delete(resource_path('views/welcome.blade.php'));
+        Storage::copy(__DIR__.'/../../stubs/resources/views/app.blade.php', resource_path('views/app.blade.php'));
+
+        // JS + Vue Pages..
+        Storage::copy(__DIR__.'/../../stubs/resources/js/app.js', resource_path('js/app.js'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('js/Pages'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/js/Pages', resource_path('js/Pages'));
+
+        // Routes..
+        $this->info('Install Routes..');
+        Storage::copy(__DIR__.'/../../stubs/routes/web.php', base_path('routes/web.php'));
+
+        // Vite config..
+        $this->info('Install Vite Config..');
+        Storage::copy(__DIR__.'/../../stubs/vite.config.js', base_path('vite.config.js'));
+
+        $this->info('Run npm install..');
+        $this->runCommands(['npm install']);
+
+        $this->info('Template installed successfully !!');
+    }
+
+    /**
+     * Update the "package.json" file.
+     *
+     * @param  callable  $callback
+     * @param bool $dev
+     * @return void
+     */
+    protected static function updateNodePackages(callable $callback, bool $dev = true): void
+    {
+        if (! file_exists(base_path('package.json'))) {
+            return;
+        }
+
+        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $packages[$configurationKey] = $callback(
+            array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [],
+            $configurationKey
+        );
+
+        ksort($packages[$configurationKey]);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
+        );
+    }
+
+    /**
+     * Install the middleware to a group in the application Http Kernel.
+     *
+     * @param string $after
+     * @param string $name
+     * @param string $group
+     * @return void
+     */
+    protected function installMiddlewareAfter(string $after, string $name, string $group = 'web'): void
+    {
+        $httpKernel = file_get_contents(app_path('Http/Kernel.php'));
+
+        $middlewareGroups = Str::before(Str::after($httpKernel, '$middlewareGroups = ['), '];');
+        $middlewareGroup = Str::before(Str::after($middlewareGroups, "'$group' => ["), '],');
+
+        if (! Str::contains($middlewareGroup, $name)) {
+            $modifiedMiddlewareGroup = str_replace(
+                $after.',',
+                $after.','.PHP_EOL.'            '.$name.',',
+                $middlewareGroup,
+            );
+
+            file_put_contents(app_path('Http/Kernel.php'), str_replace(
+                $middlewareGroups,
+                str_replace($middlewareGroup, $modifiedMiddlewareGroup, $middlewareGroups),
+                $httpKernel
+            ));
+        }
+    }
+
+    /**
+     * Run the given commands.
+     *
+     * @param array $commands
+     * @return void
+     */
+    protected function runCommands(array $commands): void
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
+    }
+}
---
+++ src/LaravelTemplateServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace QuentinKay\LaravelTemplate;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTemplateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (!$this->app->runningInConsole())
+        {
+            return;
+        }
+
+        $this->commands([
+            Console\InstallCommand::class,
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return [Console\InstallCommand::class];
+    }
+}
